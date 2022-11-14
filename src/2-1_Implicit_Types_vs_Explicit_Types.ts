63404e3b343f7cf0811781aad4a2630966adc7bd
sangwl9 <sangwl9@uci.edu>
2022-11-14 13:54:07 +0900

    #2.1 Implicit Types vs Explicit Types

---
+++ src/2-1_Implicit_Types_vs_Explicit_Types.ts
@@ -0,0 +1,16 @@
+// Implicit Types
+let a = "hello";
+a = "bye";
+// a = 1
+
+//Explicit Types
+let b: boolean = true;
+// b = "x"
+
+let c: number[] = [1, 2, 3, 4];
+// c.push("1")
+
+const player1 = {
+    name: "nico",
+};
+// player1.hello();
