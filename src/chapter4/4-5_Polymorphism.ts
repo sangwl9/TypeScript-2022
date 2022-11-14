ebc313f07df02e8ec4035fa93d3cbe77167e58b2
sangwl9 <sangwl9@uci.edu>
2022-11-14 19:58:00 +0900

    #4.5 Polymorphism

---
+++ src/chapter4/4-5_Polymorphism.ts
@@ -0,0 +1,30 @@
+interface SStorage<T> {
+    [key: string]: T;
+}
+
+class LocalStorage<T> {
+    private storage: SStorage<T> = {};
+    set(key: string, value: T) {
+        this.storage[key] = value;
+    }
+    remove(key: string) {
+        delete this.storage[key];
+    }
+    get(key: string): T {
+        return this.storage[key];
+    }
+    clear() {
+        this.storage = {};
+    }
+}
+
+const stringStorage = new LocalStorage<string>();
+
+stringStorage.get("ket");
+stringStorage.set("sangwoo", "lee");
+
+const booleanStorage = new LocalStorage<boolean>();
+
+booleanStorage.get("xx");
+//booleanStorage.set("sangwoo", "lee");
+booleanStorage.set("sangwoo", true);
